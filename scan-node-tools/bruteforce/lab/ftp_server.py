0d5d0da65ed65f347704427f390eb23894b2209d
cyb3r-Kn1ght <23520928@gm.uit.edu.vn>
2025-08-20 17:15:20 +0700

    feat: update bruteforce + ffuf-entry

---
+++ scan-node-tools/bruteforce/lab/ftp_server.py
@@ -0,0 +1,6 @@
+﻿from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+a=DummyAuthorizer(); a.add_user("dev","Summer2025!","/tmp",perm="elr")
+h=FTPHandler; h.authorizer=a; h.passive_ports=range(21000,21006)
+FTPServer(("0.0.0.0",2121),h).serve_forever()

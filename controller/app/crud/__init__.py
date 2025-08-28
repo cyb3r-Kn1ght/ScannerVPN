c10857324cbf405a9fadccb7f652e9acd2c49f83
nguyenminhnguyen <23521046@gm.uit.edu.vn>
2025-08-28 13:13:44 +0700

    fix bug

--- controller/app/crud/__init__.py
+++ controller/app/crud/__init__.py
@@ -1,3 +1,4 @@
+from . import crud_scan_job, crud_scan_result, crud_vpn_profile, crud_workflow
 
 from .crud_scan_job import *
 from .crud_scan_result import *
---
+++ controller/app/services/__init__.py
@@ -0,0 +1,1 @@
+from . import admin_service, result_service, scan_job_service, scan_submission_service, vpn_service, workflow_service
